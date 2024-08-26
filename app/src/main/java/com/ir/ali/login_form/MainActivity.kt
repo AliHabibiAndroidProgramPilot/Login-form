2e4dfd3427c1fbe177c607f7ddd9f8090b32ecbe
Ali <biz.alihabibi@gmail.com>
2024-08-26 10:04:04 +0330

    Setup viewBinding

--- app/src/main/java/com/ir/ali/login_form/MainActivity.kt
+++ app/src/main/java/com/ir/ali/login_form/MainActivity.kt
@@ -2,10 +2,13 @@ package com.ir.ali.login_form
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.ir.ali.login_form.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }
