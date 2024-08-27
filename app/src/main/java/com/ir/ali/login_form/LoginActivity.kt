e54e098ae67150658a467052f7a0b0b84b5b3e1c
Ali <biz.alihabibi@gmail.com>
2024-08-27 11:05:43 +0330

    Text Input Fileds Optimized to react to errors

--- app/src/main/java/com/ir/ali/login_form/LoginActivity.kt
+++ app/src/main/java/com/ir/ali/login_form/LoginActivity.kt
@@ -2,6 +2,7 @@ package com.ir.ali.login_form
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import com.ir.ali.login_form.databinding.ActivityLoginBinding
 
 class LoginActivity : AppCompatActivity() {
@@ -10,5 +11,42 @@ class LoginActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityLoginBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        binding.btnSignIn.setOnClickListener {
+            val email: String = binding.editText1.text.toString().trim()
+            val password: String = binding.editText2.text.toString().trim()
+            if(emailValidation(email) && passwordValidation(password)) {
+                Toast.makeText(this, "SIGNED IN", Toast.LENGTH_LONG).show()
+            }
+        }
+    }
+
+    private fun passwordValidation(password: String): Boolean {
+        if (password.isEmpty()) {
+            binding.passwordEdittext.error = "Password Can not be Empty"
+            return false
+        }
+        else {
+            if (password.length < 8) {
+                binding.passwordEdittext.error = "Password Should be more than 8 Character"
+                return false
+            }
+        }
+        binding.passwordEdittext.error = null
+        return true
+    }
+
+    private fun emailValidation(email: String): Boolean {
+        if (email.isEmpty()) {
+            binding.emailEdittext.error = "Email Can not be Empty"
+            return false
+        }
+        else {
+            if (!android.util.Patterns.EMAIL_ADDRESS.matcher(email).matches()) {
+                binding.emailEdittext.error = "Enter Valid Email"
+                return false
+            }
+        }
+        binding.emailEdittext.error = null
+        return true
     }
 }
